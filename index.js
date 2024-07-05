b59c01764da62b67b07388e9ae3282ba381093eb
Kilany <y.m.elkilany@gmail.com>
2024-07-05 18:40:09 +0300

    config: Configure PORT for production

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post(baseUrl, (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`)
 })
